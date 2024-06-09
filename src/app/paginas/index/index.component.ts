ba85eae6cf4ec2d6c8333283947d8833f1004faa
BRAMDON <Brandon18092003>
2024-06-09 12:05:02 -0500

    cambio campanita

--- src/app/paginas/index/index.component.ts
+++ src/app/paginas/index/index.component.ts
@@ -9,7 +9,7 @@ import { AlertService } from '../../services/alert.service';
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
-export class IndexComponent implements OnInit{
+export class IndexComponent implements OnInit {
   currentView: string = 'link1';
   private breakpointObserver = inject(BreakpointObserver);
   hasPinnedAlert$: Observable<boolean>;
@@ -19,13 +19,42 @@ export class IndexComponent implements OnInit{
   }
 
   ngOnInit(): void {
-      if (typeof window !== 'undefined'){
-        const saveView = localStorage.getItem('IndexcurrentView');
-        if(saveView){
-          this.currentView = saveView;
-        }
+    if (typeof window !== 'undefined') {
+      const saveView = localStorage.getItem('IndexcurrentView');
+      if (saveView) {
+        this.currentView = saveView;
       }
+    }
+    /*this.notifications = [];*/
   }
+
+
+  //********Notificaciones******/
+
+  showNotifications = false;
+  notifications = [
+    { icon: 'fas fa-warning', title: 'Te han añadido a un nuevo grupo' },
+    { icon: 'fas fa-warning', title: 'Te han añadido a un nuevo grupo' },
+  ];  // Asumimos que este es el arreglo que representa tu "base de datos estática"
+
+  toggleNotifDropdown() {
+    this.showNotifications = !this.showNotifications;
+  }
+
+  openNotification(notification: any) {
+    console.log('Notificación abierta:', notification);
+    // Eliminar la notificación del arreglo
+    this.notifications = this.notifications.filter(n => n !== notification);
+    // Cambiar la vista actual a 'link2'
+    this.setView('link2');
+    // Opcionalmente, cerrar el menú de notificaciones
+    this.showNotifications = false;
+  }
+
+
+
+
+  //****************/
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -34,19 +63,19 @@ export class IndexComponent implements OnInit{
     );
 
 
-    setView(view: string){
-      this.currentView = view;
-      localStorage.setItem('IndexcurrentView', view);
-    }
+  setView(view: string) {
+    this.currentView = view;
+    localStorage.setItem('IndexcurrentView', view);
+  }
 
-    goToProfile() {
-      // Lógica para ir a la página de perfil
-      console.log('Ir a perfil');
-    }
-  
-    logout() {
-      // Lógica para cerrar sesión
-      console.log('Cerrar sesión');
-    }
-  
+  goToProfile() {
+    // Lógica para ir a la página de perfil
+    console.log('Ir a perfil');
+  }
+
+  logout() {
+    // Lógica para cerrar sesión
+    console.log('Cerrar sesión');
+  }
+
 }
